e64c17a897a2d938e763d9c9b3729a726cc5a3db
Wilfred Tyler Gee <wtylergee@gmail.com>
2023-06-26 11:42:48 -1000

    * Deal with nan arm flux a bit better.

--- python/pfs/qa/detectorMapQa.py
+++ python/pfs/qa/detectorMapQa.py
@@ -445,12 +445,6 @@ class PlotResidualTask(Task):
             dmapReservedFiber = dmapReserved.query(f'fiberId == {f}')
             dmapReservedFiberNoTrace = dmapReservedFiber.query('Trace == False')
 
-            pfsArmFlux = pfsArm.flux[pfsArm.fiberId == f]
-            if not all(np.isnan(pfsArmFlux)):
-                pfsArmFlux = np.nanmedian(pfsArmFlux)
-            else:
-                pfsArmFlux = np.nan
-
             dictvalues = [
                 len(dmapUsedFiber),
                 len(dmapReservedFiber),
@@ -464,7 +458,7 @@ class PlotResidualTask(Task):
                 np.nanmedian(dmapReservedFiber.dx),
                 np.nanmedian(dmapUsedFiberNoTrace.dy),
                 np.nanmedian(dmapReservedFiberNoTrace.dy),
-                pfsArmFlux,
+                pd.DataFrame(pfsArm.flux[pfsArm.fiberId == f].T).median().values[0],
             ]
             for k, v in zip(dictkeys, dictvalues):
                 statistics[k] = np.append(statistics[k], v)
