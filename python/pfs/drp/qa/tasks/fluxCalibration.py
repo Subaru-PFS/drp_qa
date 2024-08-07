b163d95fe230b82bcd542da3f845f9c48f089c00
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-08-07 08:14:07 -1000

    * Remove the last item out of the `helpers` module.

--- python/pfs/drp/qa/tasks/fluxCalibration.py
+++ python/pfs/drp/qa/tasks/fluxCalibration.py
@@ -11,9 +11,8 @@ from matplotlib import pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.lines import Line2D
 from pfs.datamodel import PfsConfig, PfsSingle, TargetType
-from pfs.drp.qa.utils.helpers import get_filter_curves
 from pfs.drp.qa.utils.plotting import div_palette, spectrograph_plot_markers
-from pfs.drp.stella.fitReference import TransmissionCurve
+from pfs.drp.stella.fitReference import FilterCurve, TransmissionCurve
 from pfs.drp.stella.utils.math import robustRms
 
 
@@ -372,3 +371,43 @@ def plot_flux_cal_mag_diff(
     fig.suptitle(title)
 
     return fig
+
+
+def get_filter_curves(filter_set="ps1", include_fake_j=False) -> Dict[str, TransmissionCurve]:
+    """Get the filter curves for the given filter set.
+
+    Parameters
+    ----------
+    filter_set : str
+        Filter set to use.
+    include_fake_j : bool
+        Whether to include a fake narrow J filter. Defaults to False.
+
+    Returns
+    -------
+    filter_curves : dict[str, TransmissionCurve]
+        Filter curves.
+    """
+    filter_curves = dict()
+    for filter_name in FilterCurve.filenames.keys():
+        if not filter_name.endswith(filter_set.lower()):
+            continue
+
+        fc = FilterCurve(filter_name)
+        fc.filter_name = filter_name
+        filter_curves[filter_name] = fc
+
+    if include_fake_j:
+        # Add a fake narrow J filter that is a copy of the PS1 y filter.
+        fakeJ_wl = np.arange(1082, 1202)
+
+        # Fake J filter is 1 everywhere except for the edges.
+        fakeJ_transmission = np.ones_like(fakeJ_wl)
+        fakeJ_transmission[:1] = 0.0
+        fakeJ_transmission[-2:] = 0.0
+
+        fakeJ = TransmissionCurve(fakeJ_wl, fakeJ_transmission)
+        fakeJ.filter_name = "fakeJ_ps1"
+        filter_curves[fakeJ.filter_name] = fakeJ
+
+    return filter_curves
