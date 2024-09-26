42bb13d2435b8c0dfea3af01b1be96b089fab0c0
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-09-26 12:41:32 -1000

    Now start adjusting the quantums

--- python/pfs/drp/qa/detectorMapQa.py
+++ python/pfs/drp/qa/detectorMapQa.py
@@ -1,5 +1,7 @@
+import typing
 from typing import Iterable
 
+from lsst.daf.butler import DataCoordinate, DatasetRef
 from lsst.pex.config import ConfigurableField
 from lsst.pipe.base import (
     InputQuantizedConnection,
@@ -10,7 +12,7 @@ from lsst.pipe.base import (
     QuantumContext,
     Struct,
 )
-from lsst.pipe.base.connectionTypes import Input as InputConnection, Output as OutputConnection
+from lsst.pipe.base.connectionTypes import (BaseInput, Input as InputConnection, Output, Output as OutputConnection)
 from pfs.drp.stella import ArcLineSet, DetectorMap
 
 from pfs.drp.qa.tasks.detectorMapResiduals import PlotResidualTask
@@ -33,6 +35,52 @@ class DetectorMapQaConnections(
         else:
             del self.dmQaCombinedResidualPlot
             del self.dmQaDetectorStats
+
+    def adjustQuantum(
+        self,
+        inputs: typing.Dict[str, typing.Tuple[BaseInput, typing.Collection[DatasetRef]]],
+        outputs: typing.Dict[str, typing.Tuple[Output, typing.Collection[DatasetRef]]],
+        label: str,
+        data_id: DataCoordinate,
+    ) -> typing.Tuple[
+        typing.Mapping[str, typing.Tuple[BaseInput, typing.Collection[DatasetRef]]],
+        typing.Mapping[str, typing.Tuple[Output, typing.Collection[DatasetRef]]],
+    ]:
+        """Adjust the connections for a single quantum.
+
+        This method is called by the `QuantumContext` to adjust the connections
+        for a single quantum. The default implementation simply returns the
+        inputs and outputs unchanged.
+
+        Parameters
+        ----------
+        inputs : dict
+            The inputs for the quantum.
+        outputs : dict
+            The outputs for the quantum.
+        label : str
+            The label for the quantum.
+        data_id : DataCoordinate
+            The data ID for the quantum.
+
+        Returns
+        -------
+        dict
+            The adjusted inputs for the quantum.
+        dict
+            The adjusted outputs for the quantum.
+        """
+        print(f"adjustQuantum: {label=} {data_id=}")
+        print(f"adjustQuantum: {inputs=}")
+        print(f"adjustQuantum: {outputs=}")
+        if self.plotResidual.config.combineVisits:
+            del outputs["dmQaResidualPlot"]
+            del outputs["dmQaResidualStats"]
+        else:
+            del outputs["dmQaCombinedResidualPlot"]
+            del outputs["dmQaDetectorStats"]
+
+        return inputs, outputs
 
     detectorMaps = InputConnection(
         name="detectorMap_calib",
