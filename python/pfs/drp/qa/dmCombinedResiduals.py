628f4b935a3041047ba6eaf312733647bfd05af3
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-10-17 14:59:41 -1000

    Adjust storageClass for output type fix

--- python/pfs/drp/qa/dmCombinedResiduals.py
+++ python/pfs/drp/qa/dmCombinedResiduals.py
@@ -11,6 +11,7 @@ from lsst.pipe.base import (
 )
 from lsst.pipe.base.connectionTypes import (
     Input as InputConnection,
+    Output as OutputConnection,
 )
 from matplotlib import pyplot as plt
 from pandas import DataFrame
@@ -21,7 +22,7 @@ from pfs.drp.qa.utils.storageClasses import MultipagePdfFigure
 
 class DetectorMapCombinedQaConnections(
     PipelineTaskConnections,
-    dimensions=("instrument", "arm", "spectrograph"),
+    dimensions=("instrument",),
 ):
     """Connections for DetectorMapCombinedQaTask"""
 
@@ -38,19 +39,19 @@ class DetectorMapCombinedQaConnections(
         multiple=True,
     )
 
-    # dmQaCombinedResidualPlot = OutputConnection(
-    #     name="dmQaCombinedResidualPlot",
-    #     doc="The 1D and 2D residual plots of the detectormap with the arclines for all detectors.",
-    #     storageClass="MultipagePdfFigure",
-    #     dimensions=("instrument",),
-    # )
-    #
-    # dmQaDetectorStats = OutputConnection(
-    #     name="dmQaDetectorStats",
-    #     doc="Statistics of the residual analysis for all detectors.",
-    #     storageClass="DataFrame",
-    #     dimensions=("instrument",),
-    # )
+    dmQaCombinedResidualPlot = OutputConnection(
+        name="dmQaCombinedResidualPlot",
+        doc="The 1D and 2D residual plots of the detectormap with the arclines for all detectors.",
+        storageClass="MultipagePdfFigure",
+        dimensions=("instrument",),
+    )
+
+    dmQaDetectorStats = OutputConnection(
+        name="dmQaDetectorStats",
+        doc="Statistics of the residual analysis for all detectors.",
+        storageClass="DataFrame",
+        dimensions=("instrument",),
+    )
 
 
 class DetectorMapCombinedQaConfig(PipelineTaskConfig, pipelineConnections=DetectorMapCombinedQaConnections):
@@ -99,8 +100,7 @@ class DetectorMapCombinedQaTask(PipelineTask):
             fig = plot_detector_exposures(stats, ccd)
             pdf.append(fig)
 
-        # return Struct(dmQaCombinedResidualPlot=pdf, dmQaDetectorStats=stats)
-        return Struct()
+        return Struct(dmQaCombinedResidualPlot=pdf, dmQaDetectorStats=stats)
 
 
 def plot_detector_exposures(data, ccd):
