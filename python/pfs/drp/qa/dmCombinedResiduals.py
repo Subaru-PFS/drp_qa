49fcc1b5ee1a91783d00ff7a0dd3db2f83457ba8
Wilfred Tyler Gee <wtylergee@gmail.com>
2025-02-27 11:09:41 -1000

    Don't show all visits as it gets too long.

--- python/pfs/drp/qa/dmCombinedResiduals.py
+++ python/pfs/drp/qa/dmCombinedResiduals.py
@@ -219,14 +219,13 @@ def make_report(
             arm = ccd[0]
             spec = int(ccd[1])
             data = arc_data.query(f"arm == '{arm}' and spectrograph == {spec}")
-            visits = sorted(set(data.visit.unique()))
 
             # If we are doing a combined report we want to get the mean across visits.
             grouped = data[plot_cols].groupby(["status", "isLine", "fiberId", "y"])
             data = grouped.mean().reset_index()
 
             residFig = plot_detectormap_residuals(data, detectorMaps[ccd])
-            residFig.suptitle(f"DetectorMap Residuals - {ccd}\nMedian of {visits=}", weight="bold")
+            residFig.suptitle(f"DetectorMap Residuals - Median of all visits - {ccd}", weight="bold")
             pdf.append(residFig, dpi=150)
 
             # Add the description per visit breakdown.
